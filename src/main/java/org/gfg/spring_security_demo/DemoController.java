5493c725c541b25db89591adcc54e352a2f951b8
kajal <glotrakajal02@gmail.com>
2024-06-09 17:04:17 +0530

    security

---
+++ src/main/java/org/gfg/spring_security_demo/DemoController.java
@@ -0,0 +1,13 @@
+package org.gfg.spring_security_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "demo";
+    }
+}
